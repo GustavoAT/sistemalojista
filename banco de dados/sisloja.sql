8904621a57c1f208851377425e24864df9783f5c
Gustavo <gustavoalmeidateixeira@gmail.com>
2017-08-18 11:28:27 -0300

    alteração para conformidade com o código da classe de sessions

--- banco de dados/sisloja.sql
+++ banco de dados/sisloja.sql
@@ -664,12 +664,12 @@ DROP TABLE IF EXISTS `pos_sessions`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `pos_sessions` (
-  `session_id` varchar(40) NOT NULL DEFAULT '0',
+  `id` varchar(40) NOT NULL DEFAULT '0',
   `ip_address` varchar(16) NOT NULL DEFAULT '0',
   `user_agent` varchar(120) NOT NULL,
-  `last_activity` int(10) unsigned NOT NULL DEFAULT '0',
-  `user_data` text,
-  PRIMARY KEY (`session_id`)
+  `timestamp` int(10) unsigned NOT NULL DEFAULT '0',
+  `data` text,
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
