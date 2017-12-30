fbb375f637a527349914ed655ceabed947a46f92
Gustavo <gustavoalmeidateixeira@gmail.com>
2017-12-30 07:32:43 -0300

    ajustes no css para caber no header

--- application/views/partial/header.php
+++ application/views/partial/header.php
@@ -61,19 +61,19 @@ html {
 		</div>
 
 		<div id="menubar_date">
-		<?php
- 
-$dia = date('d'); // Dia do mês com 2 digitos
-$s = date('w'); // Representação numérica dos dias da semana.
-$m = date('n'); // Repressentação numérica sem o zero.
-$ano = date('Y');
- 
-$semana = array(0=>"Domingo", 1=>"Segunda", 2=>"Terça", 3=>"Quarta", 4=>"Quinta", 5=>"Sexta", 6=>"Sábado");
-$mes = array(1=>"Janeiro","Fevereiro","Março","Abril","Maio","Junho","Julho","Agosto","Setembro","Outubro","Novembro","Dezembo");
- 
-echo "<p>$semana[$s], $dia de $mes[$m] de  $ano</p>";
- 
-?>
+			<?php
+	
+				$dia = date('d'); // Dia do mês com 2 digitos
+				$s = date('w'); // Representação numérica dos dias da semana.
+				$m = date('n'); // Repressentação numérica sem o zero.
+				$ano = date('Y');
+				
+				$semana = array(0=>"Domingo", 1=>"Segunda", 2=>"Terça", 3=>"Quarta", 4=>"Quinta", 5=>"Sexta", 6=>"Sábado");
+				$mes = array(1=>"Janeiro","Fevereiro","Março","Abril","Maio","Junho","Julho","Agosto","Setembro","Outubro","Novembro","Dezembo");
+				
+				echo "<p>$semana[$s], $dia de $mes[$m] de  $ano</p>";
+				
+			?>
 		</div>
 
 	</div>
